5bf5197923a30afb8fc8f1cb526287fbc475b41a
afisman <afisman@gmail.com>
2024-05-11 22:20:39 +0200

    fixed styles

--- assets/scripts/swipers/roomsSwiper.js
+++ assets/scripts/swipers/roomsSwiper.js
@@ -24,7 +24,7 @@ const roomsSwiper = new Swiper('.RoomsGridSection__swiper', {
                 rows: 2,
                 fill: 'row'
             },
-            centeredSlides: true
+            centeredSlides: false
         }
     }
 });
